9ae3d6f492e201723a9dbf4a89641ae99bf85c71
syedtalhaaskari <88083531+syedtalhaaskari97@users.noreply.github.com>
2023-07-31 23:50:46 +0500

    Complete step01

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 "use strict";
-let message = "Hello World";
-message = 6;
-console.log(message);
+//strongly typed syntax
+let a = "Pakistan";
+a = "USA";
+let b = 9;
+let c = true;
+//type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
--- app.ts
+++ app.ts
@@ -1,3 +1,12 @@
-let message = "Hello World";
-message = 6;
-console.log(message);
+//strongly typed syntax
+let a : string = "Pakistan";
+a = "USA";
+let b : number = 9;
+let c : boolean = true;
+
+//type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
