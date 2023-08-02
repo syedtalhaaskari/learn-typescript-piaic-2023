27ac79555e1c6012a6593eec996fc2ae63fe5af8
syedtalhaaskari <88083531+syedtalhaaskari97@users.noreply.github.com>
2023-08-03 00:27:19 +0500

    Complete step04

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-#! /usr/bin/env node
-import chalk from "chalk";
-import inquirer from "inquirer";
-let answers = await inquirer.prompt([
-    {
-        name: "age",
-        type: "number",
-        message: "Enter your Age:"
-    }
-]);
-console.log(chalk.blue("Insha Allah, in " + (60 - answers.age) + " years you will be 60 years old."));
+let myname;
+myname = null;
+console.log(myname);
+myname = "zia";
+console.log(myname);
+//myname = undefined; //Error
+//myname = 12; //Error
+let myAge;
+myAge = 16; //narrowing
+console.log(myAge);
+//console.log(myAge.toLowerCase());//Error
+myAge = "Dont Know"; //narrowing
+console.log(myAge);
+console.log(myAge.toString()); // common to both types 
+//can be called even without narrowing
+console.log(myAge.toLowerCase()); //Can be called on string 
+//because of narrowing
+let newAge = Math.random() > 0.6 ? "Khan" : 60;
+//newAge.toLowerCase();//Error: Transpiler cannot narrow
+if (newAge === "Khan") {
+    // Type of newAge: string
+    newAge.toUpperCase(); // Can be called
+}
+if (typeof newAge === "string") {
+    // Type of newAge: string
+    newAge.toUpperCase(); // Can be called
+}
+typeof newAge === "string"
+    ? newAge.toUpperCase() // Ok: string
+    : newAge.toFixed(); // Ok: number
+let age;
+age = 90; //OK
+age = "died"; //OK
+age = "unknown"; //OK
+//age = "living";//Error
+let zia;
+zia = "zia";
+//zia = "khan";//Error
+let yourName = Math.random() > 0.6 ? "Hira Khan" : undefined;
+if (yourName) {
+    yourName.toUpperCase(); // Ok: string
+}
+//yourName.toUpperCase();//Error: Object is possibly 'undefined'.
+yourName?.toUpperCase(); //OK
+let data;
+export {};
